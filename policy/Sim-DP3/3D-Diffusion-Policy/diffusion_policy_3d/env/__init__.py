04232507380b15e882baaaf50d8ef7606f2a55b4
mechapuru <puru4robo@gmail.com>
2026-03-23 15:31:26 +0530

    corrected sim dp3

---
+++ policy/Sim-DP3/3D-Diffusion-Policy/diffusion_policy_3d/env/__init__.py
@@ -0,0 +1,20 @@
+
+# try:
+#     from .adroit import AdroitEnv
+# except ImportError:
+#     pass
+
+# try:
+#     from .dexart import DexArtEnv
+# except ImportError:
+#     pass
+
+# try:
+#     from .metaworld import MetaWorldEnv
+# except Exception:
+#     pass
+
+
+
+
+from .pybullet.xarm6_env import XArm6PickPlaceEnv
